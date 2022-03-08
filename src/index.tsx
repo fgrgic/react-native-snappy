01734ea735bc7217d4cdc2cc5381fa97901e18c3
Fran Grgić <grgicfran@gmail.com>
2022-03-08 14:50:58 +0100

    :zap: Expose all icons to public

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
-import Snappy from './Snappy'
-import { SnappyIconNames } from './Snappy/types'
+import Snappy from './Snappy';
+import { SnappyIconNames } from './Snappy/types';
+import AllIcons from './Snappy/svg/index';
 
-export { SnappyIconNames }
+export { SnappyIconNames, AllIcons };
 
-export default Snappy
+export default Snappy;
 
 // export default NativeModules.RNSnappyModule
